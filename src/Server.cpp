2d39c32434c88025d1e561f33c42d3bc88d1b2e6
lin <120377843@qq.com>
2022-02-20 20:53:23 +0800

    finished gtest

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,11 +1,18 @@
 #include "httplib/httplib.h"
 #include "Server.h"
+#include "Logger.h"
+#include "lib/Reverse.h"
 
 void Server::start(){
     httplib::Server svr;
+    Logger logger;
+    Reverse reverse;
 
-    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
-        res.set_content("hell world\n", "text/plain");
+    svr.Get("/hi", [&](const httplib::Request &, httplib::Response &res) {
+        logger.print("handle request");
+        std::string string = "hell world\n";
+        reverse.reverse(string);
+        res.set_content(string, "text/plain");
     });
 
     svr.listen("0.0.0.0", 8080);
--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,10 +1,17 @@
 #include <string>
 #include <gtest/gtest.h>
+#include "Reverse.h"
 
 class UtilTests : public ::testing::Test
 {
 };
 
 TEST_F(UtilTests, simple){
-    EXPECT_EQ(3L, 3L);
+    std::string toRev = "Hello";
+
+    Reverse rev;
+    std::string res = rev.reverse(toRev);
+
+    EXPECT_EQ(res, "olleH" );
+    EXPECT_EQ(1, 1 );
 }
